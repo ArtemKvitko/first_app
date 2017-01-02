450116df811710ac15ddca93bd685313b5729bda
artem <k.artem.v@gmail.com>
2017-01-02 19:40:56 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ FirstApp2::Application.routes.draw do
 
   # get "users/new"
   root 'static_pages#home'
-  match '/singup',  to: 'users#new',            via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   resources :microposts
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
 
 
