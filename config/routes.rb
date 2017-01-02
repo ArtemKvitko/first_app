17a2ed13b22bc92b75db72db6ebf5952842ae7ec
artem <k.artem.v@gmail.com>
2017-01-02 17:30:33 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 FirstApp2::Application.routes.draw do
 
-  get "users/new"
+  # get "users/new"
   root 'static_pages#home'
   match '/singup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   resources :microposts
+  resources :users
 
 
 
